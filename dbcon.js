5afb99f3c2d56ed594f98b75c8de39dc550e82df
Colin Joss <colindjoss@gmail.com>
2021-06-01 11:57:48 -0700

    Edited password

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
 connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs290_jossc',
-    password        : '4414',
+    password        : 'takingUpSpac3',
     database        : 'cs290_jossc'
 });
 
